80c820483d3797f329f07eeed057b156659845e1
Cefas <gondolasaurus@gmail.com>
2021-12-10 14:41:08 +0100

    Errors thrown en tests erbij.

--- Frontend/AngularFrontend/src/app/upload-cursussen/upload-cursussen.component.spec.ts
+++ Frontend/AngularFrontend/src/app/upload-cursussen/upload-cursussen.component.spec.ts
@@ -37,8 +37,8 @@ describe('UploadCursussenComponent', () => {
   }));
 
   it('should slice an array of strings by a certain length', fakeAsync(() => {
-    let strArr: Array<string> = ['1', '2', '3', '4'];
-    expect(component.SliceSplitFile(strArr, 0, 2)).toEqual(['1', '2']);
+    let strArr: Array<string> = ['1', '2', '3', '4', '5', '6'];
+    expect(component.SliceSplitFile(strArr, 0)).toEqual(['1', '2', '3', '4', '5']);
   }));
 
   it('should change string so first part is removed', fakeAsync(() => {
@@ -73,7 +73,7 @@ Titel: Java Persistence API
 Cursuscode: JPA
 Duur: 2 dagen
 Startdatum: 15/10/2018
-    
+
 `;
 
     let expectedCursusArray: Array<Cursus> = 
@@ -83,5 +83,98 @@ Startdatum: 15/10/2018
     ]
     
     expect(component.ReformToCursussenArray(originalString)).toEqual(expectedCursusArray);
+  }));
+
+  it('should throw if string has incorrect order', fakeAsync(() => {
+    let originalString: string = 
+`Titel: C# Programmeren
+Cursuscode: CNETIN
+Duur: 3 dagen
+Startdatum: 8/10/2018
+
+Titel: Java Persistence API
+Duur: 2 dagen
+Cursuscode: JPA
+Startdatum: 15/10/2018
+
+`;
+    
+    expect(function (){ component.ReformToCursussenArray(originalString) }).toThrow('Bestand is niet in correct formaat op regel 7. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+
+  it('should throw if string has incorrect breaks', fakeAsync(() => {
+    let originalString: string = 
+`Titel: C# Programmeren
+Cursuscode: CNETIN
+Duur: 3 dagen
+Startdatum: 8/10/2018
+Titel: Java Persistence API
+Cursuscode: JPA
+Duur: 2 dagen
+Startdatum: 15/10/2018
+
+`;
+    
+    expect(function (){ component.ReformToCursussenArray(originalString) }).toThrow('Bestand is niet in correct formaat op regel 5. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+
+  it('should throw if string has incorrect date form', fakeAsync(() => {
+    let originalString: string = 
+`Titel: C# Programmeren
+Cursuscode: CNETIN
+Duur: 3 dagen
+Startdatum: 8/10/2018
+
+Titel: Java Persistence API
+Cursuscode: JPA
+Duur: 2 dagen
+Startdatum: 15-10-2018
+
+`;
+    
+    expect(function (){ component.ReformToCursussenArray(originalString) }).toThrow('Bestand is niet in correct formaat op regel 9. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+  
+  it('should throw if values do not contain string with first removable', fakeAsync(() => {
+    let originalString: string = 'Remove me: Do not remove me (Do remove me)';
+    let removable: string = 'Not there: ';
+    let secondRemovable: string = ' (Do remove me)';
+    expect(function(){ component.ThrowIfIncorrectValues(originalString, removable, 3, 2, secondRemovable); }).toThrow('Bestand is niet in correct formaat op regel 18. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+  
+  it('should throw if values do not contain string with second removable', fakeAsync(() => {
+    let originalString: string = 'Remove me: Do not remove me (Do remove me)';
+    let removable: string = 'Remove me: ';
+    let secondRemovable: string = ' (Do NOT remove me)';
+    expect(function(){ component.ThrowIfIncorrectValues(originalString, removable, 3, 2, secondRemovable); }).toThrow('Bestand is niet in correct formaat op regel 18. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+  
+  it('should throw if value does not contain string', fakeAsync(() => {
+    let originalString: string = 'Remove me: Do not remove me';
+    let removable: string = 'Not there: ';
+    expect(function(){ component.ThrowIfIncorrectValue(originalString, removable, 3, 2); }).toThrow('Bestand is niet in correct formaat op regel 18. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+
+  it('should throw if value contains anything', fakeAsync(() => {
+    let originalString: string = 'Anything at all';
+    expect(function(){ component.ThrowIfNotEmpty(originalString, 3, 2); }).toThrow('Bestand is niet in correct formaat op regel 18. Er zijn geen cursusinstanties toegevoegd.');
+  }));
+  
+  it('should not throw if values do contain strings', fakeAsync(() => {
+    let originalString: string = 'Remove me: Do not remove me (Do remove me)';
+    let removable: string = 'Remove me: ';
+    let secondRemovable: string = ' (Do remove me)';
+    expect(function(){ component.ThrowIfIncorrectValues(originalString, removable, 3, 2, secondRemovable); }).not.toThrow();
+  }));
+  
+  it('should not throw if value does contain string', fakeAsync(() => {
+    let originalString: string = 'Remove me: Do not remove me.';
+    let removable: string = 'Remove me: ';
+    expect(function(){ component.ThrowIfIncorrectValue(originalString, removable, 3, 2); }).not.toThrow();
+  }));
+
+  it('should not throw if value contains nothing', fakeAsync(() => {
+    let originalString: string = '';
+    expect(function(){ component.ThrowIfNotEmpty(originalString, 3, 2); }).not.toThrow();
   }));
 });
--- Frontend/AngularFrontend/src/app/upload-cursussen/upload-cursussen.component.ts
+++ Frontend/AngularFrontend/src/app/upload-cursussen/upload-cursussen.component.ts
@@ -11,6 +11,10 @@ import { Cursus } from '../models/Cursus';
 export class UploadCursussenComponent implements OnInit {
   public file: any;
   public answerToPost: any;
+  public invoerenErrorMessage: String = '';
+    
+  private fileTypes = ['txt'];
+  private cursusArrayLength: number = 5;
 
   constructor(private cursuservice: CursusService) { }
 
@@ -18,7 +22,21 @@ export class UploadCursussenComponent implements OnInit {
   }
   
   fileChanged(file: any) {
-    this.file = file.target.files[0];
+    if(this.fileIsAllowed(file.target)){
+      this.invoerenErrorMessage = '';
+      this.file = file.target.files[0];
+    } else {
+      this.invoerenErrorMessage = 'Bestand is niet in correct formaat.';
+    }
+  }
+
+  fileIsAllowed(input: any): boolean {
+    if (input.files && input.files[0]) {
+        var extension = input.files[0].name.split('.').pop().toLowerCase(),  
+            isSuccess = this.fileTypes.indexOf(extension) > -1;
+        return isSuccess; 
+    }
+    return false;
   }
 
   postCursus(cursussen: Array<Cursus>){
@@ -26,12 +44,26 @@ export class UploadCursussenComponent implements OnInit {
   }
 
   uploadDocument() {
+    this.invoerenErrorMessage = '';
     let fileReader = new FileReader();
-    fileReader.onload = (e) => {
+    fileReader.onload = () => {
       const FullFile = fileReader.result;
       
       if(FullFile != null){
-        this.postCursus(this.ReformToCursussenArray(FullFile));
+        try{
+          this.postCursus(this.ReformToCursussenArray(FullFile));
+          this.invoerenErrorMessage = '';
+        } catch(error){
+          let message;
+
+          if (error instanceof String) {
+            message = error;
+          } else {
+            message = String(error);
+          }
+
+          this.invoerenErrorMessage = message;
+        }
       }
     }
     fileReader.readAsText(this.file);
@@ -40,14 +72,15 @@ export class UploadCursussenComponent implements OnInit {
   ReformToCursussenArray(fullFile: any): Array<Cursus>{
     let splitFile: Array<string> = this.SplitFullFile(fullFile);
     let cursusArrays: Array<Array<string>> = [];
-    let cursusArrayLength: number = 5;
     let cursussen: Array<Cursus> = [];
 
-    for (let i = 0; i < splitFile.length; i+=cursusArrayLength) {
-      cursusArrays.push(this.SliceSplitFile(splitFile, i, cursusArrayLength));
+    for (let i = 0; i < splitFile.length; i+=this.cursusArrayLength) {
+      if(this.SliceSplitFile(splitFile, i).length > 1) {
+        cursusArrays.push(this.SliceSplitFile(splitFile, i));
+      }
     }
     for (let i = 0; i < cursusArrays.length; i++){
-      for (let e = 0; e < cursusArrayLength; e++){
+      for (let e = 0; e < this.cursusArrayLength; e++){
         if (cursusArrays[i][e] != null){
           switch (e){
             case 0:
@@ -58,23 +91,32 @@ export class UploadCursussenComponent implements OnInit {
                 duur: 0,
                 cursusInstanties: []
               });
+              this.ThrowIfIncorrectValue(cursusArrays[i][e], 'Titel: ', i, e);
+
               cursussen[i].titel = this.RemoveTextFromCursusPart(cursusArrays[i][e], 'Titel: ', '', '');
             break;
             case 1:
+              this.ThrowIfIncorrectValue(cursusArrays[i][e], 'Cursuscode: ', i, e);
+
               cursussen[i].code = this.RemoveTextFromCursusPart(cursusArrays[i][e], 'Cursuscode: ', '', '');
             break;
             case 2:
+              this.ThrowIfIncorrectValues(cursusArrays[i][e], 'Duur: ', i, e, ' dagen');
+
               cursussen[i].duur = +(this.RemoveTextFromCursusPart(cursusArrays[i][e], 'Duur: ', ' dagen', ''));
             break;
             case 3:
-              cursussen[i].cursusInstanties.push({id: 0, startDatum: new Date(this.RemoveTextFromCursusPart(cursusArrays[i][e], 'StartDatum: ', /(\d+[/])(\d+[/])/, '$2$1') )});
+              this.ThrowIfIncorrectValues(cursusArrays[i][e], 'Startdatum: ', i, e, '/');
+
+              cursussen[i].cursusInstanties.push({id: 0, startDatum: new Date(this.RemoveTextFromCursusPart(cursusArrays[i][e], 'Startdatum: ', /(\d+[/])(\d+[/])/, '$2$1') )});
+            break;
+            case 4:
+              this.ThrowIfNotEmpty(cursusArrays[i][e], i, e);
             break;
           }
         }
       }
     }
-
-    cursussen.pop();
     return cursussen;
   }
 
@@ -82,11 +124,29 @@ export class UploadCursussenComponent implements OnInit {
     return fullFile.toString().split('\n');
   }
 
-  SliceSplitFile(splitFile: Array<string>, i: number, cursusArrayLength: number): Array<string>{
-    return splitFile.slice(i, i+cursusArrayLength);
+  SliceSplitFile(splitFile: Array<string>, i: number): Array<string>{
+    return splitFile.slice(i, i+this.cursusArrayLength);
   }
 
   RemoveTextFromCursusPart(originalValue: string, stringToBeRemoved: string, secondStringToBeRemoved:  string | RegExp, secondStringToBeReplacement: string): string{
     return originalValue.replace(stringToBeRemoved, '').replace(secondStringToBeRemoved, secondStringToBeReplacement);
+  }
+
+  ThrowIfIncorrectValue(value: string, removable1: string, i: number, e: number) {
+    if(!value.includes(removable1)){
+      throw 'Bestand is niet in correct formaat op regel ' + String(i*this.cursusArrayLength + e + 1) + '. Er zijn geen cursusinstanties toegevoegd.'
+    }
+  }
+
+  ThrowIfIncorrectValues(value: string, removable1: string, i: number, e: number, removable2: string) {
+    if(!value.includes(removable1) || !value.includes(removable2)){
+      throw 'Bestand is niet in correct formaat op regel ' + String(i*this.cursusArrayLength + e + 1) + '. Er zijn geen cursusinstanties toegevoegd.'
+    }
+  }
+
+  ThrowIfNotEmpty(value: string, i: number, e: number){
+    if(value){
+      throw 'Bestand is niet in correct formaat op regel ' + String(i*this.cursusArrayLength + e + 1) + '. Er zijn geen cursusinstanties toegevoegd.'
+    }
   }
 }
